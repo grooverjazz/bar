591d7048604355bd4d1f12edd9bab49b19325041
sjerpsthomas <sjerpsthomas@gmail.com>
2024-06-05 09:54:38 +0200

    Add Apache POI dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.poi)
+    implementation(libs.poi.ooxml)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
