972438ee1381206bab447f8b0a603f82b6890c7a
sjerpsthomas <sjerpsthomas@gmail.com>
2024-06-10 16:24:19 +0200

    Add confirmation dialog for removing items

--- app/src/main/java/org/groover/bar/app/beheer/items/BeheerItemsScreen.kt
+++ app/src/main/java/org/groover/bar/app/beheer/items/BeheerItemsScreen.kt
@@ -13,15 +13,22 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material.icons.rounded.KeyboardArrowUp
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.groover.bar.data.item.Item
@@ -63,6 +70,23 @@ private fun BeheerItemsContent(
         modifier = Modifier
             .padding(10.dp)
     ) {
+        var itemRemoveState: Item? by remember { mutableStateOf(null) }
+
+        if (itemRemoveState != null) {
+            PopupDialog(
+                confirmText = "Verwijderen",
+                dismissText = "Annuleren",
+                onConfirm = {
+                    itemRemove(itemRemoveState!!.id)
+                    itemRemoveState = null
+                },
+                onDismiss = { itemRemoveState = null },
+                dialogTitle = "Item verwijderen",
+                dialogText = "Weet je zeker dat je dit item (${itemRemoveState!!.name}) wilt verwijderen?",
+                icon = Icons.Rounded.Delete,
+            )
+        }
+
         // Terug button
         NavigateButton(
             navigate = navigate,
@@ -76,15 +100,9 @@ private fun BeheerItemsContent(
         TitleText("Items")
         Spacer(modifier = Modifier.size(20.dp))
 
-
         // Add item button
         Button(onClick = {
-            addItem(
-                "Tijdelijk item",
-                Cents(0),
-                0,
-                0,
-            )
+            addItem("Tijdelijk item", Cents(0), 0, 0,)
         }) {
             Text("Add item")
         }
@@ -93,7 +111,10 @@ private fun BeheerItemsContent(
 
         // Items edit list
         LazyColumn(
-            modifier = Modifier.padding(10.dp).background(Color.LightGray).height(800.dp)
+            modifier = Modifier
+                .padding(10.dp)
+                .background(Color.LightGray)
+                .height(800.dp)
         ) {
             items.forEach { item ->
                 item {
@@ -103,7 +124,7 @@ private fun BeheerItemsContent(
                         onTitleClick = { navigate("beheer/items/item/${item.id}") },
                         onUpClick = { itemMoveUp(item.id) },
                         onDownClick = { itemMoveDown(item.id) },
-                        onDeleteClick = { itemRemove(item.id) }
+                        onDeleteClick = { itemRemoveState = item }
                     )
                 }
             }
@@ -121,7 +142,9 @@ fun ItemEditItem(
     onDeleteClick: () -> Unit,
 ) {
     Column(
-        modifier = Modifier.padding(vertical = 5.dp).fillMaxWidth()
+        modifier = Modifier
+            .padding(vertical = 5.dp)
+            .fillMaxWidth()
     ) {
         Button(modifier = Modifier
             .fillMaxWidth()
@@ -159,4 +182,33 @@ fun ItemEditItem(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PopupDialog(
+    confirmText: String,
+    dismissText: String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = { Icon(icon, null) },
+        title = { Text(text = dialogTitle) },
+        text = { Text(dialogText) },
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(dismissText)
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(confirmText)
+            }
+        }
+    )
 }
