234705ff01c2c7ad65fd271a0bd2104f8bf6f460
sjerpsthomas <sjerpsthomas@gmail.com>
2025-01-04 16:46:38 +0100

    Add scale to bar layout

--- app/src/main/java/org/groover/bar/app/util/BarLayout.kt
+++ app/src/main/java/org/groover/bar/app/util/BarLayout.kt
@@ -2,11 +2,13 @@ package org.groover.bar.app.util
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 
 /**
@@ -14,8 +16,14 @@ import androidx.compose.ui.unit.dp
  */
 @Composable
 fun BarLayout(content: @Composable ColumnScope.() -> Unit) {
-    Column(Modifier.fillMaxWidth().padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        content = content
-    )
+    // Change scale of app if needed
+    val scale = 1f
+    val density = LocalDensity.current.density * scale
+
+    CompositionLocalProvider(LocalDensity provides Density(density = density)) {
+        Column(Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            content = content
+        )
+    }
 }
